c715497c2c06cfd40b7b1822b77fcce5b2cb2f1a
Zsuzsa.L <manoaway@gmail.com>
2024-02-22 11:57:45 +0100

    adjust image path for production

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,53 +1,54 @@
-import ReactMarkdown from 'react-markdown'
-import Layout from "../components/layout"
+import ReactMarkdown from "react-markdown";
+import Layout from "../components/layout";
 import { useState, useEffect } from "react";
 
-const About = ()=>{
+const About = () => {
+  const [aboutData, setAboutData] = useState({ Biography: "", Info: "" });
+  const [imgData, setImgData] = useState({ alternativeText: "", url: "" });
 
+  useEffect(() => {
+    // update the about data
+    // when the component is rendered for the first time
+    get();
+  }, []);
 
-      const [aboutData, setAboutData] = useState({Biography:'', Info:''});
-      const [imgData, setImgData] = useState({alternativeText:'', url:''});
+  // This function updates the component with the
+  // current about data stored in the server
+  function get() {
+    fetch(`${process.env.REACT_APP_BACKEND}api/abouts?populate=*`)
+      .then((res) => res.json())
+      .then((about) => {
+        setAboutData(about.data[0].attributes);
+        setImgData(about.data[0].attributes.Peti.data.attributes);
+      });
+  }
 
-    
-      useEffect(() => {
-        // update the about data
-        // when the component is rendered for the first time
-        get();
-      }, []);
-    
-      // This function updates the component with the
-      // current about data stored in the server
-      function get() {
-        fetch(`${process.env.REACT_APP_BACKEND}api/abouts?populate=*`)
-          .then(res => res.json())
-          .then(about => {
-           setAboutData(about.data[0].attributes);
-setImgData(about.data[0].attributes.Peti.data.attributes)
+  const bio = aboutData.Biography;
+  const info = aboutData.Info;
+  const alternativeText = imgData.alternativeText;
 
-          })
-      }
+  return (
+    <Layout>
+      <section className="about" id="about">
+        <div className="wrapper">
+          <p>{bio}</p>
+          <button>
+            <a href="/contact">Contact Me</a>
+          </button>
+        </div>
+        <div className="image">
+          <img
+            src={imgData?.url.substring(1)}
+            alt={alternativeText}
+            id="peti"
+          />
+          <div className="card-text">
+            <ReactMarkdown>{info}</ReactMarkdown>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
-  
-      const bio = aboutData.Biography
-      const info = aboutData.Info
-      const alternativeText = imgData.alternativeText
-
-
-return <Layout>
- <section className="about" id="about">
-    <div className="wrapper">
-    <p>{bio}</p>   
-  <button><a href="/contact">Contact Me</a></button>
-    </div>
-    <div className="image">
-      <img src={process.env.REACT_APP_BACKEND + imgData?.url.substring(1)} alt={alternativeText} id="peti"/>
-      <div className="card-text">
-        <ReactMarkdown>{info}</ReactMarkdown>
-       
-      </div>
-    </div>
-  </section>
-  </Layout>
-}
-
-export default About
+export default About;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,55 +1,74 @@
 import { useState, useEffect } from "react";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
+const Contact = () => {
+  const [email, setEmail] = useState("");
+  // from the image data I will use only the url and the alt text
+  const [imgData, setImgData] = useState({ alternativeText: "", url: "" });
 
-const Contact = ()=>{
+  useEffect(() => {
+    // update the contact data
+    // when the component is rendered for the first time
+    get();
+  }, []);
 
-      const [email, setEmail] = useState("");
-      // from the image data I will use only the url and the alt text
-      const [imgData, setImgData] = useState({alternativeText:'', url:''});
+  // This function updates the component with the
+  // current contact data stored in the server
+  function get() {
+    fetch(`${process.env.REACT_APP_BACKEND}api/contacts?populate=*`)
+      .then((res) => res.json())
+      .then((contact) => {
+        setEmail(contact.data[0].attributes.Email);
+        //I save the image data separatly as it is on a deeper level
+        setImgData(contact.data[0].attributes.Image.data.attributes);
+      });
+  }
 
-     
-    
-      useEffect(() => {
-        // update the contact data
-        // when the component is rendered for the first time
-        get();
-      }, []);
-    
-      // This function updates the component with the
-      // current contact data stored in the server
-      function get() {
-        fetch(`${process.env.REACT_APP_BACKEND}api/contacts?populate=*`)
-          .then(res => res.json())
-          .then(contact => {
-            setEmail(contact.data[0].attributes.Email);
-          //I save the image data separatly as it is on a deeper level
-            setImgData(contact.data[0].attributes.Image.data.attributes)
-          })
-      }
+  const alternativeText = imgData.alternativeText;
 
-const alternativeText = imgData.alternativeText
+  //the base url ends with '/' and the url of the img starts with one, so I have to remove one of them
+  const url = imgData?.url.substring(1);
 
-//the base url ends with '/' and the url of the img starts with one, so I have to remove one of them
-const url = process.env.REACT_APP_BACKEND + imgData?.url.substring(1)
+  return (
+    <Layout>
+      <section className="contact">
+        <div className="wrapper">
+          <form
+            action="https://formspree.io/myynvoaj"
+            method="POST"
+            className="form"
+          >
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+            <input
+              type="email"
+              id="email"
+              name="name"
+              placeholder="Email"
+              required
+            />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+              required
+            ></textarea>
+            <input type="submit" className="submit" value="SEND" />
+          </form>
+          <span className="or">or</span>
+          <p className="email">{email}</p>
+        </div>
+        <div className="image">
+          <img src={url} alt={alternativeText} />
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
-return <Layout>
-  <section className="contact">
-    <div className="wrapper">
-  <form action="https://formspree.io/myynvoaj" method="POST" className="form">
-    <input type="text" id="name" name="name"  placeholder="Name" required/>
-    <input type="email" id="email" name="name" placeholder="Email" required/>
-    <textarea name="message" id="message" placeholder="Message" required></textarea>
-    <input type="submit" className="submit" value="SEND"/>
-    </form>
-    <span className="or">or</span>
-    <p className="email">{email}</p>
-      </div>
-      <div className="image">
-        <img src={url} alt={alternativeText} />
-      </div>
-  </section>
-  </Layout>
-}
-
-export default Contact
+export default Contact;
